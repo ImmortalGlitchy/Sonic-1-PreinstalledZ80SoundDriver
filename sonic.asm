6c3b68b8569272e380d542fd2ee2f708cfd70991
ImmortalGlitchy <theglitched234@gmail.com>
2023-12-30 00:25:48 +0000

    Stuff

--- sonic.asm
+++ sonic.asm
@@ -16,7 +16,7 @@ AddressSRAM	  = 3	; 0 = odd+even; 2 = even only; 3 = odd only
 ; Change to 0 to build the original version of the game, dubbed REV00
 ; Change to 1 to build the later vesion, dubbed REV01, which includes various bugfixes and enhancements
 ; Change to 2 to build the version from Sonic Mega Collection, dubbed REVXB, which fixes the infamous "spike bug"
-Revision	  = 0
+Revision	  = 1
 
 ZoneCount	  = 6	; discrete zones are: GHZ, MZ, SYZ, LZ, SLZ, and SBZ
 
@@ -152,7 +152,7 @@ RomEndLoc:	dc.l EndOfRom-1		; End address of ROM
 EndOfHeader:
 
 ; ===========================================================================
-; Crash/Freeze the 68000. Unlike Sonic 2, Sonic 1 uses the 68000 for playing music, so it stops too
+; Crash/Freeze the 68000. Like sonic 2, the sound driver is now Z80 based, thus the music continues playing.
 
 ErrorTrap:
 		nop	
